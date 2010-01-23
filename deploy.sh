0557aab4a6b479730b4e0887310b163c260c8362
Simon Home <simon@hysear.(none)>
2010-01-23 11:23:47 +0100

    Updated the deploy script to push the mdb app to tomcat

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 mvn clean install webstart:jnlp
-rsync -avz --rsh=ssh --progress ./target/jnlp/ hancock:/home/sat/workspace/musicdatabase/app
+rsync -avz --rsh=ssh --progress ./target/jnlp/ sat@edip:/home/sat/websites/mdb/
